6f4027bc39d14cea4e7081fa547c884eaf0000b3
satoru2727 <kshdudjaodhdu@gmail.com>
2024-07-22 10:53:58 +0000

    initial commit!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+async function(key ,url) {
+const params = {method : "POST", body : JSON.stringify({key:key,url:url})};
+const baseURL = "https://xn--s7y.xn--tckwe/api/short"
+return await fetch(params,baseURL);
+}
+</script>
+
+
